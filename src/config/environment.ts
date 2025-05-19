f0dddfed990aff2cd3134fd14b2f2fb8973a76a5
AntRed <anthonyatras@gmail.com>
2025-05-19 15:24:35 -0400

    Arreglando tema de insfraestructura

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   apiUrl:
     process.env.NODE_ENV === 'production'
-      ? 'https://artilesphotography.com/api'
-      : 'http://localhost:8080/api',
+      ? 'http://localhost/api'
+      : 'http://localhost/api',
 };
