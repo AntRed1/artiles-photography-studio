752d1500169129fef9d4a5a3dda3d19f82ac05d2
Anthony J. Rojas Valdez <109046137+AntRed1@users.noreply.github.com>
2025-07-21 12:53:04 -0400

    Actualizar environment.ts
    Agregando el dominio https para la prueba de Railway: https://artiles-photography-backend.up.railway.app/

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: process.env.NODE_ENV === 'production',
   apiUrl:
     process.env.NODE_ENV === 'production'
-      ? 'http://10.0.16.173:8080/api'
-      : 'http://localhost:8080/api',
+      ? 'https://artiles-photography-backend.up.railway.app/'
+      : 'https://artiles-photography-backend.up.railway.app/',
 };
