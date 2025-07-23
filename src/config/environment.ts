b63e42a7a2deb6bbbad8e78e4cd67c9cedcbcbb4
AntRed <anthonyatras@gmail.com>
2025-07-23 11:09:40 -0400

    Agregando configuracion

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: process.env.NODE_ENV === 'production',
   apiUrl:
     process.env.NODE_ENV === 'production'
-      ? 'https://artiles-photography-backend.up.railway.app/'
-      : 'https://artiles-photography-backend.up.railway.app/',
+      ? 'https://api-photoquince-backend.up.railway.app/api'
+      : 'https://api-photoquince-backend.up.railway.app/api',
 };
